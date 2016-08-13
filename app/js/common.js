221a0395c0ac0f4ce959fc6d7e1e0480f962bcbb
georgiy-g <grigoliya.g@gmail.com>
2016-08-14 01:57:50 +0300

    six

--- app/js/common.js
+++ app/js/common.js
@@ -45,21 +45,44 @@ $(function() {
 	$(".ask-question-nav-mob .icon-close").click(function () {
 		$(".ask-question-nav-mob").css("right","-100%");
 	});
+
 	//header-menu-clicked
 	$(".cost__block").click(function () {
 		var n = $(".cost__block").index($(this));
-		$(".cost__block").removeClass("active-block");
-		$(".cost__block").eq(n).addClass("active-block");
-		
+		$(".cost__block").removeClass("active-block ");
+		$(".cost__block").eq(n).addClass("active-block");	
+	});
+
+	//about-insurance
+	$(".description-blocks").first().addClass("active-description");
+	$(".about-name ul li a").first().addClass("active-list");
+	$(".about-name ul li a").click(function (e) {
+		e.preventDefault();
+		var n = $(".about-name ul li a").index($(this));
+		$(".description-blocks").removeClass("active-description");
+		$(".about-name ul li a").removeClass("active-list");
+		$(".about-name ul li a").eq(n).addClass("active-list");
+		$(".description-blocks").removeClass("active-description animated fadeIn");
+		$(".description-blocks").eq(n).addClass("active-description animated fadeIn");	
 	});
 
 	//slider for employees mobile
 	$(".employees-slider_container").slick({
 		dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-	})
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	});
+	//slider for about-insurance
+	$(".about__slider").slick({
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		arrows: true,
+		prevArrow: '<div class="slick-arrow slick-prev"></div>',
+		nextArrow: '<div class="slick-arrow slick-next"></div>',
+		slidesToScroll: 1,
+	});
 
 });
